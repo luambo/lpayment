e6ab93e60615aa27498395a72e22ec1e571997a9
unknown <luambo1@hotmail.com>
2024-03-15 11:53:25 -0300

    change from option to checkbox, update total value amount based on checked boxes.

--- js/escripto.js
+++ js/escripto.js
@@ -1,27 +1,39 @@
 // Função para atualizar o valor com base na seleção da modalidade e revelar outras divs
 
-const amountBallet = 60;
-const amountJazz = 65;
-const amountSalao= 70;
+const defaultAmount = 60;
+const amountSalao = 70;
 const number = 21983619249;
-function updateValue() {
-    var modality = document.getElementById("modality").value;
-    if (modality !== "") {
-        var amount = amountBallet;
-        //var total_amount = 0;
-        if (modality === "jazz"){
-            amount = amountJazz;
-        } else if (modality === "salao") {
-            amount = amountSalao;
-        }
+const desconto = 10;
 
+function updateValue() {
+    var checkboxes = document.querySelectorAll('input[name="modalities[]"]');
+    var amount = 0;
+    var update = 0;
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            update++;
+            if (checkbox.value === "salao") {
+                amount += amountSalao;
+            } else {
+                amount += defaultAmount;
+            }
+        }
+    });
+
+    if (update > 1) {
+        amount -= update * desconto; 
+    }
+
+    if (amount > 0) {
         document.getElementById("display-amount").textContent = amount.toFixed(2);
         document.getElementById("payment-amount").style.display = "block";
         document.getElementById("pix-estabelecimento").style.display = "block";
         document.getElementById("extra-info").style.display = "block";
         document.getElementById("pay-btn").style.display = "block";
-    }        
-    document.getElementById("amount").textContent = amount.toFixed(2);
+
+        document.getElementById("amount").textContent = amount.toFixed(2);
+    } 
+
 }
 function copyPix(){
     const element = document.getElementById('pix');
@@ -36,17 +48,26 @@ function copyPix(){
         })
 }
 
-
-// Função de envio do formulário
 function submitWhats() {
+    
     document.getElementById("payment-form").addEventListener("submit", function (event) {
         event.preventDefault();
+        var checkboxes = document.querySelectorAll('input[name="modalities[]"]');
+        var modalidades = "";
+        checkboxes.forEach(checkbox => {
+           if (checkbox.checked) {
+                modalidades += checkbox.value + ", ";
+           }
+        });
+        modalidades = modalidades.slice(0,-2);
         var name = document.getElementById("name").value;
-        var modality = document.getElementById("modality").value;
+        var valor = document.getElementById("display-amount").textContent;
         var message = `**Pagamento de mensalidade**
 Nome completo: ${name}
 
-Modalidade: *${modality}*
+Modalidade: *${modalidades}*
+
+Valor: *${valor}*
 
 *Comprovante: a enviar...*`;
 
